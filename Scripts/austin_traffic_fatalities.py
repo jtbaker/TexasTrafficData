3b79890f5c366eef9094b79984ba54b139eccde7
jtbaker <jason.th.baker@gmail.com>
2018-01-18 18:09:50 -0600

    Requirements.txt commit

---
+++ Scripts/austin_traffic_fatalities.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import fiona
+import geopandas as gpd
+import folium
+from folium import GeoJson
+from shapely.geometry import Point
+
+# Reading our Fatality Data csv's from the Austin Open Data Portal into some pandas Dataframes.
+f_2015 = pd.read_csv('2015_APD_Traffic_Fatalities.csv')
+f_2016 = pd.read_csv('2016_APD_Traffic_Fatalities.csv')
+
+# Getting Austin local zip codes, to winnow down our Census Tract data on later by running a spatial join.
+zippath='zipcodes/geo_export_f636f682-5b8a-41eb-9ec4-cfb9f60bfdf2.shp'
+zips = gpd.GeoDataFrame.from_file(zippath)
+zips.geometry = zips['geometry']
+zips = zips.loc[zips['name'].isin(['TRAVIS','AUSTIN','PFLUGERVILLE','DEL VALLE',
+                                   'MANOR','MANCHACA','LEANDER', 'CEDAR PARK'])]
+# Setting coordinate system
+zips.crs = fiona.crs.from_epsg(4326)
+
+# Opening the Texas census tract layer from the directory the zip file created.
+census_tracts = gpd.read_file('tl_2017_48_tract/tl_2017_48_tract.shp')
+census_tracts.crs = fiona.crs.from_epsg(4326)
+# Spatial Join to access only the local census tracts.
+census_tracts = gpd.tools.sjoin(zips, census_tracts, how='left', op='within')
+
+gdf1 = gpd.GeoDataFrame(f_2015)
+gdf2 = gpd.GeoDataFrame(f_2016)
+# Cleansing the data so we have the same key values to work with.
+gdf2 = gdf2.rename({'Ran Red Light or Stop Sign':'Ran Red Light','charge_slater': 'charge','restraint / helmet':'restraint'},axis='columns')
+
+# Appending our initial dataframe to the 2nd one, after matching the key values to each other.
+gdf = gdf2.append(gdf1)
+# Time conversion..
+# gdf['datetime'] = pd.to_datetime(gdf['Time'])
+# Adding our values to a
+
+gdf['X COORD'], gdf['Y COORD'] = pd.to_numeric(gdf['X COORD']), pd.to_numeric(gdf['Y COORD'])
+geometry = [Point(xy) for xy in zip(gdf['X COORD'], gdf['Y COORD'])]
+# Setting geometry
+gdf['geometry'] = geometry
+gdf.crs = fiona.crs.from_epsg(4326)
+
+# Adding a pandas timestamped field for easy searchability
+gdf['datetime'] = pd.to_datetime(gdf['Date']+' '+gdf['Time'])
+
+
+Incidents2016 = gdf.loc[gdf['datetime']>='2016-01-01']
+Incidents2015 = gdf.loc[gdf['datetime']<='2015-12-31']
+Assaults = gdf.loc[(gdf['charge'].str.contains('agg', case=False, na=False)) | (gdf['charge'].str.contains('manslaughter',case=False, na=False)) | (gdf['charge'].str.contains('homicide', case=False, na=False))]
+No_Seatbelts = gdf.loc[gdf['restraint'].str.contains('no seatbelt', case=False, na = False)]
+Invalid_DL = gdf.loc[gdf['DL Status'] != 'ok']
+Ran_Red = gdf.loc[gdf['Ran Red Light'] == 'Y']
+Speeding = gdf.loc[gdf['Speeding'] == 'Y']
+Daytime = gdf.loc[(gdf.Hour >= 6) & (gdf.Hour <= 7)]
+Nighttime = gdf.loc[(gdf.Hour < 6) | (gdf.Hour > 7)]
+
+sub_cats = {'2015 Incidents':{'data':Incidents2015, 'color':'blue'},
+            '2016 Incidents':{'data':Incidents2016, 'color':'red'},
+            'Assaults':{'data':Assaults,'color':'red'},
+            'No Seatbelts':{'data': No_Seatbelts,'color':'red'},
+            'Invalid DL':{'data':Invalid_DL,'color':'blue'},
+            "Ran Red Light":{'data':Ran_Red,'color':'red'},
+            "Speeding":{'data':Speeding,'color':'orange'},
+            'Daytime':{'data': Daytime,'color': 'orange'},
+            'Nighttime':{'data':Nighttime,'color':'black'}}
+
+# Making copies of our census tract geodataframe.
+fts15, fts16 = census_tracts.copy(), census_tracts.copy()
+# Adding the number of points in each census tract polygon, using GeoPandas.GeoSeries.contains method. If a point
+# falls on the boundary of a census tract polygon(as wanton on roadway data), it will get counted as a point for both
+# census tract polygons.
+fts15['NUMPOINTS'] = fts15['geometry'].apply(lambda x: len([x.contains(point) for point in tuple(Incidents2015.geometry) if x.contains(point) is True]))
+fts16['NUMPOINTS'] = fts16['geometry'].apply(lambda x: len([x.contains(point) for point in tuple(Incidents2016.geometry) if x.contains(point) is True]))
+census_tracts['NUMPOINTS'], census_tracts['delta'] = sum([fts15['NUMPOINTS'], fts16['NUMPOINTS']]), fts16['NUMPOINTS'] - fts15['NUMPOINTS']
+
+
+# Structuring our polygon geodataframes  into a dictionary.
+ctracts={'2015 Traffic Fatalities by Census Tract':fts15,'2016 Traffic Fatalities by Census Tract':fts16}
+deltas = {'2015-2016 Traffic Fatality changes':census_tracts}
+
+# Instantiating a folium Map object with our initial extent values, and the basemap we'll be using.
+map = folium.Map(location=[30.2747,-97.7407], tiles='Stamen Toner', zoom_start=11,)
+
+# Color pallette dictionary with RGB values to assign colors, based on quantity.
+color_dict = {'0':'#00000000','1':'#EBB5B9','2':'#EB8A8A','3':"#EB5E5C",'4':'#EB3735','5':'#EB1F1C','6':"#EA0707",
+              '7':'#BE003A','8':"#AA0034",'9':"#810029",'10':"#55001D",'11':"#55001D",'12':"#55001D",'13':"#55001D",
+              '14':"#55001D"}
+
+# Writing a function to add GeoDataFrames to the map object. Passes a dictionary object, which stores the data behind a
+# string value, which will prove useful. Builds a folium FeatureGroup for each geodataframe, zips the values we specify
+# into a tuple we can iterate on, and plot the values on the Map.
+def add_layers(dictobject):
+    for geodataframe in dictobject:
+        local=dictobject[geodataframe]
+        fg = folium.FeatureGroup(name=geodataframe)
+        fg.layer_name = geodataframe
+        try:
+            data = local['data']
+            for x,y,charge,time,date in zip(data['X COORD'], data['Y COORD'], data['charge'], data['Time'], data['Date']):
+                folium.Marker([y,x], popup=f"Category: {geodataframe}<br>Charge: {charge}<br>Date: {pd.to_datetime(date).strftime('''%b %d, %Y''')}"+'<br>'+'Time: '+time, icon=folium.Icon(color=local['color'])).add_to(fg)
+        # Handling non-point objects(our census tract polygon layers), and adding them to the feature group (fg).
+        except KeyError:
+            GeoJson(local,
+                    highlight_function=lambda feature: {'weight': 3,
+                                                          'fillOpacity': 0.8,
+                                                          'fillColor':color_dict[str(feature['properties']['NUMPOINTS'])],
+                                                          'color':"#5D0C0E",
+                                                          'borderOpacity': 0.0,
+                                                          },
+                    style_function=lambda feature:{'weight': 2,
+                                                          'fillOpacity': 0.6,
+                                                          'fillColor':color_dict[str(feature['properties']['NUMPOINTS'])],
+                                                          'color':"#423e41",
+                                                          'borderOpacity': 0.0,
+                                                          },
+                    smooth_factor=2.0,
+            ).add_to(fg)
+            # adding a popup to show the details of each tract.
+            for geo, pts in zip(local.geometry, local['NUMPOINTS']):
+                # Adding a marker with the details for each polygon.
+                folium.Marker([geo.centroid.y, geo.centroid.x],
+                              popup=f"{pts} {'fatalities' if pts != 1 else 'fatality'} in this tract during {geodataframe[:4]}",
+                              icon=folium.Icon(icon='bookmark', color='green')
+                ).add_to(fg)
+        map.add_child(fg, name=geodataframe)
+
+
+add_layers(ctracts)
+add_layers(sub_cats)
+folium.LayerControl(autoZIndex=True).add_to(map)
+map.save('austinfatalities.html')
---
+++ Scripts/dpsregions.py
@@ -0,0 +1,140 @@
+import folium
+import pandas as pd
+import geopandas as gpd
+import fiona
+from branca.utilities import split_six
+
+# DPS Regions dictionary. Values copied and pasted from the bottom portion of each region's public facing web page on https://www.dps.texas.gov/
+dpsregions = {
+    "DPS Region 1":'Anderson, Bowie, Camp, Cass, Cherokee, Collin, Cooke, Dallas, Delta, Denton, Ellis, Erath, Fannin, Franklin, Grayson, Gregg, Harrison, Henderson, Hood, Hopkins, Hunt, Johnson, Kaufman, Lamar, Marion, Morris, Panola, Navarro, Palo Pinto, Parker, Rains, Red River, Rockwall, Rusk, Smith, Somervell, Tarrant, Titus, Upshur, Van Zandt, Wise, Wood',
+    "DPS Region 2":'Angelina, Austin, Brazoria, Brazos, Burleson, Chambers, Colorado, Fort Bend, Galveston, Grimes, Hardin, Harris, Houston, Jasper, Jefferson, Leon, Liberty, Madison, Matagorda, Montgomery, Nacogdoches, Newton, Orange, Polk, Robertson, Sabine, San Augustine, San Jacinto, Shelby, Trinity, Tyler, Walker, Waller, Washington, Wharton',
+    "DPS Region 3":'Aransas, Bee, Brooks, Cameron, Dimmit, Duval, Edwards, Hidalgo, Jim Hogg, Jim Wells, Kenedy, Kinney, Kleberg, LaSalle, Live Oak, Maverick, Nueces, Real, Refugio, San Patricio, Starr, Webb, Willacy, Uvalde, Val Verde, Zapata, Zavala',
+    "DPS Region 4":'Andrews, Borden, Brewster, Coke, Concho, Crane, Crockett, Culberson, Dawson, Ector, El Paso, Gaines, Glasscock, Howard, Hudspeth, Irion, Jeff Davis, Kimble, Loving, Mason, Martin, McCulloch, Menard, Midland, Pecos, Presidio, Reagan, Reeves, Schleicher, Sterling, Sutton, Terrell, Tom Green, Upton, Ward, Winkler',
+    "DPS Region 5":'Archer, Armstrong, Bailey, Baylor, Briscoe, Brown, Callahan, Carson, Castro, Clay, Childress, Cochran, Coleman, Collingsworth, Comanche, Cottle, Crosby, Dallam, Deaf Smith, Dickens, Donley, Eastland, Fisher, Floyd, Foard, Garza, Gray, Hale, Hall, Hansford, Hardeman, Hartley, Haskell, Hemphill, Hockley, Hutchinson, Jack, Jones, Kent, King, Knox, Lamb, Lipscomb, Lubbock, Lynn, Mitchell, Montague, Moore, Motley, Nolan, Ochiltree, Oldham, Parmer, Potter, Randall, Roberts, Runnels, Scurry, Shackleford, Sherman, Stephens, Stonewall, Swisher, Taylor, Terry, Throckmorton, Wheeler, Wichita, Wilbarger, Yoakum, Young',
+    "DPS Region 6":'Atascosa, Bandera, Bastrop, Bell, Bexar, Blanco, Bosque, Burnet, Caldwell, Calhoun, Comal, Coryell, DeWitt, Falls, Fayette, Freestone, Frio, Gillespie, Goliad, Gonzales, Guadalupe, Hamilton, Hays, Hill, Jackson, Karnes, Kendall, Kerr, Lampasas, Lavaca, Lee, Limestone, Llano, McMullen, Medina, Milam, Mills, McLennan, San Saba, Travis, Victoria, Williamson, Wilson',
+}
+
+# Converting the long strings to a list
+for region in dpsregions:
+    dpsregions[region]=dpsregions[region].split(', ')
+
+# Building a list of lists assigning the DPS Region into a list with each county that is listed under it.
+for region in dpsregions:
+    dpsregions[region]=[[region, county] for county in dpsregions[region]]
+
+# Flattened list object to feed into a Dataframe
+dps_regions_list = [item for key in dpsregions for item in dpsregions[key]]
+
+# Reading in some census data
+census_data = pd.read_excel('Dtl2010hcat.xls')
+
+# Cleaning up the data (syntax)
+census_data = census_data[['Area','Total']].loc[(census_data['AgeGroup'] == "'ALL'") & (census_data['Area'] != 'Texas')]
+census_data=census_data.rename(columns={'Area':'NAME', 'Total':'Total Population'})
+census_data['NAME'].loc[census_data['NAME'] == 'DE WITT'] = 'DEWITT'
+census_data['NAME'] = census_data['NAME'].str.upper()
+
+# Reading in a county shapefile layer, cutting it down, and cleaning it up.
+counties = gpd.read_file('tl_2011_us_county/tl_2011_us_county.shp')
+counties = counties.loc[counties['STATEFP']=='48']
+counties['NAME'] = counties['NAME'].str.upper()
+
+# More cleaning ( spelling ;-) ) and syntax.
+dps_regions_list = gpd.GeoDataFrame(dps_regions_list, columns=['DPSRegion', 'NAME'])
+dps_regions_list['NAME'].loc[dps_regions_list['NAME'] == 'Shackleford'] = 'Shackelford'
+dps_regions_list['NAME'].loc[dps_regions_list['NAME'] == 'LaSalle'] = 'La Salle'
+dps_regions_list['NAME'] = dps_regions_list['NAME'].str.upper()
+
+# Dissolving the county polygons into larger Metro regions.
+stats_areas = counties.dissolve(by='CBSAFP', as_index=False)
+
+# Merging the county data with census data (Population)
+counties = counties.merge(census_data, on='NAME')
+counties = counties.merge(dps_regions_list, on='NAME', )
+counties = gpd.GeoDataFrame(counties).sort_values(by='Total Population', ascending=False)
+
+# Dissolving the county polygons into larger Metro regions.
+CBSAFP = counties.dissolve(by='CBSAFP', as_index=False, aggfunc='sum')
+
+# Dissolving the county polygons into DPS regions.
+regions = counties.dissolve(by='DPSRegion',as_index=False, aggfunc='sum')
+
+# Instantiating the folium Map object
+regionsmap = folium.Map([31.2338,-98.6768], tiles='OpenStreetMap', zoom_start=6)
+
+# Assigning a threshold scale based on Population
+thresholdscale = split_six(regions['Total Population'])
+
+# Adding a choropleth map of the DPS regions as the first layer.
+regionsmap.choropleth(
+    geo_data=regions.to_json(), data=regions, name='Texas DPS Region Polygons',columns=['DPSRegion','Total Population'],
+    legend_name= 'Texas DPS Regions by Population',key_on='feature.properties.DPSRegion', threshold_scale=thresholdscale,
+    fill_color='OrRd', fill_opacity=0.5, highlight=True, line_weight=3.0,
+)
+
+# A MSA dictonary to reverence the CBSAFP codes to
+msa_dict = {
+    "12420":"Austin-Round Rock-San Marcos, TX Metro Area",
+    "41700":"San Antonio-New Braunfels, TX Metro Area",
+    "26420":"Houston-Sugar Land-Baytown, TX Metro Area",
+    "19100":"Dallas-Fort Worth-Arlington, TX Metro Area"
+}
+
+# A function to query the county names that fall within a region.
+def get_counties_from_field(value, field):
+    return ', '.join([county for county in counties['NAMELSAD'].loc[counties[field]==value]])
+
+# A feature group for the markers with the population information for DPS regions
+fg = folium.FeatureGroup(name="Texas DPS Region Population Details")
+fg.layer_name = 'Texas DPS Region Population Details'
+for name, geo, pop in zip(regions['DPSRegion'],regions.geometry, regions['Total Population']):
+    folium.Marker([geo.centroid.y, geo.centroid.x], icon=folium.Icon(color='black'),
+                  popup=f"{'<br>'.join(['Texas '+name, 'Total Population: '+'''{:3,.0f}'''.format(pop)])}"+'<br>'+
+                        f"Includes the following Counties, highest to lowest population:<br>{get_counties_from_field(name, 'DPSRegion')}"
+                  ).add_to(fg)
+
+
+# Another feature group for the major MSAs
+pop_centers = folium.FeatureGroup(name='Texas Population Centers')
+
+# Prepping the geodataframe for projection
+CBSAFP.crs = fiona.crs.from_epsg(4326)
+CBSAFP = CBSAFP.sort_values(by="Total Population", ascending=False).iloc[:4]
+
+# Simplifying the polygon's edges to give us a more uniform feature on the map.
+CBSAFP.geometry = CBSAFP['geometry'].apply(lambda feature: feature.simplify(0.2, preserve_topology=False))
+
+# Instantiating the GEOJSON object to feed to the map
+folium.GeoJson(CBSAFP,
+    highlight_function=lambda feature: {'weight': 3,
+                                          'fillOpacity': 0.2,
+                                          'fillColor':'#4947FF',
+                                          'color':"#5D0C0E",
+                                          'borderOpacity': 0.0,
+                                          },
+    # STYLE
+    style_function=lambda feature:{'weight': 2,
+                                          'fillOpacity': 0.1,
+                                          'fillColor':'#4947FF',
+                                          'color':"#423e41",
+                                          'borderOpacity': 0.0,
+                                          },
+    smooth_factor=2.0).add_to(pop_centers)
+
+# Detail point layer for the MSAs
+for geo, cbs, pop in zip(CBSAFP.geometry, CBSAFP.CBSAFP, CBSAFP['Total Population']):
+    folium.Marker([geo.centroid.y, geo.centroid.x], icon=folium.Icon(color='white', icon_color='black'),
+                  popup=f"{'<br>'.join([msa_dict[str(cbs)], 'Total Population: ''''{:3,.0f}'''.format(pop)])}"+'<br>'+
+                        f"Includes the following Counties, highest to lowest population:<br>{get_counties_from_field(cbs, 'CBSAFP')}"
+                  ).add_to(pop_centers)
+
+# adding the feature groups to the map.
+
+fg.add_to(regionsmap)
+pop_centers.add_to(regionsmap)
+
+# Adding layer control to the map
+folium.LayerControl().add_to(regionsmap)
+
+# Saving the map to an HTML file.
+regionsmap.save('TexasDPSRegions.html')
